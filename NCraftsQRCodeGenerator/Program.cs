024924cbe9e288742b418fa45c856760ec20758f
Ismaïl <nguyen.ismail@gmail.com>
2019-05-11 13:00:39 +0200

    Add project files.

---
+++ NCraftsQRCodeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NCraftsQRCodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
